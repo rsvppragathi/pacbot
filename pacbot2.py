c224332c7eb75b8f78f1d2652f4f36dcc4a877cd
Robert Bojs <rbojs@kth.se>
2017-11-07 17:30:53 -0800

    outline, function names

---
+++ pacbot2.py
@@ -0,0 +1,34 @@
+
+
+
+def build_dqn(num_actions, action_repeat):
+
+
+
+class AtariEnvironment(object):
+
+    def __init__(self, gym_env, action_repeat):
+        pass
+    def get_initial_state(self):
+        pass
+    def get_preprocessed_frame(self, observation):
+        pass
+    def step(self, action_index):
+        pass
+
+def actor_learner(env, graph_ops, num_actions,summary_ops, saver):
+    pass
+
+def build_graph(num_actions):
+    pass
+
+def build_summaries():
+    pass
+
+def train(session, graph_ops, num_actions, saver):
+    pass
+
+def evaluation(session, graph_ops, saver):
+
+def main(_):
+    pass
