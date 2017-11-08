593699287f842a71e35b316a6e1c50c8c35845f6
Robert Bojs <rbojs@kth.se>
2017-11-07 17:37:53 -0800

    division of work

--- pacbot2.py
+++ pacbot2.py
@@ -1,10 +1,10 @@
 
-
-
+##Justin
 def build_dqn(num_actions, action_repeat):
+    pass
 
 
-
+##Micha
 class AtariEnvironment(object):
 
     def __init__(self, gym_env, action_repeat):
@@ -16,19 +16,26 @@ class AtariEnvironment(object):
     def step(self, action_index):
         pass
 
+##ALL
 def actor_learner(env, graph_ops, num_actions,summary_ops, saver):
     pass
 
+##Robert
 def build_graph(num_actions):
     pass
 
+##Robert
 def build_summaries():
     pass
 
+##Pragathi
 def train(session, graph_ops, num_actions, saver):
     pass
 
+##Pragathi
 def evaluation(session, graph_ops, saver):
 
+
+##Pragathi
 def main(_):
     pass
